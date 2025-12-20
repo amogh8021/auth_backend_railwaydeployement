bf9225e3a3524bb8670c7481fb105bcb9426e438
Amogh shrivastav <amoghdev8021@gmail.com>
2025-12-20 17:53:44 +0530

    Enable JPA and entity scanning for JwtAuth
    Added entity scanning and JPA repository enabling for JwtAuth package.

--- src/main/java/com/example/learn/JwtAuth/JwtAuthApplication.java
+++ src/main/java/com/example/learn/JwtAuth/JwtAuthApplication.java
@@ -2,12 +2,15 @@ package com.example.learn.JwtAuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.learn.JwtAuth")
+@EnableJpaRepositories(basePackages = "com.example.learn.JwtAuth")
 public class JwtAuthApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JwtAuthApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthApplication.class, args);
+    }
 }
