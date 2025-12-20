4a0dbe7fbdb9fafb1fb484f0317032c462d4ab6d
Amogh shrivastav <amoghdev8021@gmail.com>
2025-12-20 17:57:50 +0530

    Update JwtAuthApplication.java

--- src/main/java/com/example/learn/JwtAuth/JwtAuthApplication.java
+++ src/main/java/com/example/learn/JwtAuth/JwtAuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.learn.JwtAuth")
 @EntityScan(basePackages = "com.example.learn.JwtAuth")
 @EnableJpaRepositories(basePackages = "com.example.learn.JwtAuth")
 public class JwtAuthApplication {
@@ -14,3 +14,4 @@ public class JwtAuthApplication {
         SpringApplication.run(JwtAuthApplication.class, args);
     }
 }
+
